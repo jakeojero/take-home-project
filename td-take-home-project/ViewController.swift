f12a846d50f64a1b835a848c82e5b8bd0a72e2e0
Jake Ojero <jakeojero@hotmail.com>
2022-04-12 17:52:48 -0400

    Initial Commit

---
+++ td-take-home-project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  td-take-home-project
+//
+//  Created by Jake Ojero on 2022-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
